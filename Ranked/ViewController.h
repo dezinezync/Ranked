a23d34bd66332c6d560e6fa66c9a7c8be9599a67
Nikhil Nigade <nikhil@dezinezync.com>
2018-10-06 14:00:06 +0530

    initial commit

---
+++ Ranked/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ranked
+//
+//  Created by Nikhil Nigade on 06/10/18.
+//  Copyright © 2018 Nikhil Nigade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RankedTests/RankedTests.m
@@ -0,0 +1,37 @@
+//
+//  RankedTests.m
+//  RankedTests
+//
+//  Created by Nikhil Nigade on 06/10/18.
+//  Copyright © 2018 Nikhil Nigade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RankedTests : XCTestCase
+
+@end
+
+@implementation RankedTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
